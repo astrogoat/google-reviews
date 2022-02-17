78a0cf669a943db610e016e5c654cbf7a1987a36
Kristoffer Tonning <tonning@gmail.com>
2022-02-17 14:49:04 +0100

    Only include if app is enabled

--- resources/views/script.blade.php
+++ resources/views/script.blade.php
@@ -1,18 +1,12 @@
-<script src="https://apis.google.com/js/platform.js?onload=renderBadge" async defer></script>
-<script>
-
-    window.renderBadge = function() {
-
-        var ratingBadgeContainer = document.createElement("div");
-
-        document.body.appendChild(ratingBadgeContainer);
-
-        window.gapi.load('ratingbadge', function() {
-
-            window.gapi.ratingbadge.render(ratingBadgeContainer, {"merchant_id": {{settings('Astrogoat\\GoogleReviews\\Settings\\GoogleReviewsSettings', 'merchant_id')}},"position": "BOTTOM_RIGHT"});
-
-        });
-
-    }
-
-</script>
+@if(Astrogoat\GoogleReviews\Settings\GoogleReviewsSettings::isEnabled())
+    <script src="https://apis.google.com/js/platform.js?onload=renderBadge" async defer></script>
+    <script>
+        window.renderBadge = function() {
+            var ratingBadgeContainer = document.createElement("div");
+            document.body.appendChild(ratingBadgeContainer);
+            window.gapi.load('ratingbadge', function() {
+                window.gapi.ratingbadge.render(ratingBadgeContainer, {"merchant_id": {{ settings('Astrogoat\\GoogleReviews\\Settings\\GoogleReviewsSettings', 'merchant_id') }}, "position": "BOTTOM_RIGHT"});
+            });
+        }
+    </script>
+@endif
