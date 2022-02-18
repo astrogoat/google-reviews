07f41d63833aa528aec436a023e1d81e2ff288c0
Kristoffer Tonning <tonning@gmail.com>
2022-02-18 15:20:32 +0100

    Allow for $merchant_id to be a string also

--- src/Settings/GoogleReviewsSettings.php
+++ src/Settings/GoogleReviewsSettings.php
@@ -6,7 +6,7 @@ use Helix\Lego\Settings\AppSettings;
 
 class GoogleReviewsSettings extends AppSettings
 {
-    public int $merchant_id;
+    public int|string $merchant_id;
 
     protected array $rules = [
         'merchant_id' => ['required'],
